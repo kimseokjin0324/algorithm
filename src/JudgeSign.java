30c2f7d6b1cd7920dfc6dfcdd70351f9821d44c9
kimseokjin0324 <kimseokjin0324@gmail.com>
2021-11-16 17:37:49 +0900

    다중 루프 예제- 곱셈표

--- src/JudgeSign.java
+++ src/JudgeSign.java
@@ -5,12 +5,21 @@ public class JudgeSign {
 
         System.out.print("정수를 입력하세요");
         int n=stdIn.nextInt();
-
-        if(n>0)
-            System.out.printf("이 수는 양수 입니다.");
-        else if(n<0)
-            System.out.printf("이 수는 음수 입니다");
-        else
-            System.out.printf("이 수는 0입니다.");
+//        /**
+//         * 정숫값 부호예제
+//         */
+//        if(n>0)
+//            System.out.printf("이 수는 양수 입니다.");
+//        else if(n<0)
+//            System.out.printf("이 수는 음수 입니다");
+//        else
+//            System.out.printf("이 수는 0입니다.");
+        /**
+         * 리스트 1 예제
+         */
+        if(n==1) System.out.printf("이 수는 1입니다.");
+        else if(n==2) System.out.printf("이 수는 2입니다.");
+        else if(n==3) System.out.printf("이 수는 3입니다.");
+        else ;
     }
 }
