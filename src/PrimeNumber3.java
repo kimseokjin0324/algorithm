be0cb54f3976b6c1fd20941bc92ce3836e10e24c
kimseokjin0324 <kimseokjin0324@gmail.com>
2021-11-17 13:52:06 +0900

    소수의 나열-알고리즘 개선[2]

---
+++ src/PrimeNumber3.java
@@ -0,0 +1,29 @@
+class PrimeNumber3 {
+    public static void main(String[] args){
+        int counter=0;                      //곱셈과 나눗셈의 횟수
+        int ptr=0;                          //찾은 소수의 개수
+        int[] prime=new int[500];           // 소수를 저장하는 배열
+
+        prime[ptr++]=2;                     //2는 소수임
+        prime[ptr++]=3;                     //3은 소수임
+
+        for(int n=5;n<=1000;n+=2){
+            boolean flag=false;
+            for(int i=1;prime[i]*prime[i]<=n;i++){
+                counter+=2;
+                if(n%prime[i]==0){
+                    flag= true;
+                    break;
+                }
+            }
+            if(!flag){                      //마지막까지 나누어 떨어지지않음
+                prime[ptr++]=n;             //소수라고 배열에 저장
+                counter++;
+            }
+        }
+        for(int i=0;i<ptr;i++)
+            System.out.println(prime[i]);
+
+        System.out.println("곱셈과 나눗셈을 수행한 횟수 :"+counter);
+    }
+}
