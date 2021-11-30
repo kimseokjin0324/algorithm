4c4b93059156a8e5e6ee77d351bf633978b8aa14
kimseokjin0324 <kimseokjin0324@gmail.com>
2021-11-30 13:47:10 +0900

    버블 정렬 알고리즘 교환체크후 이뤄지지않으면 종료

--- src/ch06/BubbleSort.java
+++ src/ch06/BubbleSort.java
@@ -10,12 +10,19 @@ public class BubbleSort {
         a[idx2]=t;
     }
 
-    //버블 정렬
+    //버블 정렬(V2)
     static void bubbleSort(int[] a,int n){
-        for(int i=0;i<n-1;i++)
+        for(int i=0;i<n-1;i++){
+            int exchg=0;            // 패스의 교환 횟수를 기록한다.
             for(int j=n-1;j>i;j--)
-                if(a[j-1]>a[j])
+                if(a[j-1]>a[j]){
                     swap(a,j-1,j);
+                    exchg++;
+                }
+            if(exchg==0) break;     //교환이 이뤄지지 않으면 종료
+
+        }
+
     }
 
     public static void main(String[] args){
