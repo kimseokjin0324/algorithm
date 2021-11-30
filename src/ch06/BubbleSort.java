edc5e4b400e86ceae7ab7e0e1eae99253e109615
kimseokjin0324 <kimseokjin0324@gmail.com>
2021-11-30 13:51:41 +0900

    버블 정렬 알고리즘 V3

--- src/ch06/BubbleSort.java
+++ src/ch06/BubbleSort.java
@@ -10,17 +10,17 @@ public class BubbleSort {
         a[idx2]=t;
     }
 
-    //버블 정렬(V2)
+    //버블 정렬(V3)
     static void bubbleSort(int[] a,int n){
-        for(int i=0;i<n-1;i++){
-            int exchg=0;            // 패스의 교환 횟수를 기록한다.
-            for(int j=n-1;j>i;j--)
+        int k=0;
+        while(k<n-1){
+            int last=n-1;
+            for(int j=n-1;j>k;j--)
                 if(a[j-1]>a[j]){
                     swap(a,j-1,j);
-                    exchg++;
+                    last=j;
                 }
-            if(exchg==0) break;     //교환이 이뤄지지 않으면 종료
-
+            k=last;
         }
 
     }
